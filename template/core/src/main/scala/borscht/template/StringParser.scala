59258f4a52b6555e93fd07de4a2855146aa6f1e0
Eshu <tjano.xibalba@gmail.com>
2020-11-24 12:36:48 +0900

    A string parser should not be given by template module

--- template/core/src/main/scala/borscht/template/StringParser.scala
+++
@@ -1,5 +0,0 @@
-package borscht.template
-
-import borscht.Parser
-
-given ParserString(using parser: TemplateParser) as Parser[String] = parser andThen (_.render)  
--- template/core/src/main/scala/borscht/template/TemplateParser.scala
+++ template/core/src/main/scala/borscht/template/TemplateParser.scala
@@ -2,4 +2,5 @@ package borscht.template
 
 import borscht.Parser
 
-trait TemplateParser extends Parser[Template]
+trait TemplateParser extends Parser[Template]:
+  def toStringParser: Parser[String] = this andThen (_.render) 
