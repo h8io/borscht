77d09887ab4bfc81cec5d8f23d9d075eadadd81c
Eshu <tjano.xibalba@gmail.com>
2020-11-26 13:34:06 +0900

    Attribute renderers

--- template/st4/src/main/scala/borscht/template/impl/st4/ST4TemplateParser.scala
+++ template/st4/src/main/scala/borscht/template/impl/st4/ST4TemplateParser.scala
@@ -1,7 +1,20 @@
 package borscht.template.impl.st4
 
-import borscht.template.{Template, TemplateParser}
-import org.stringtemplate.v4.ST
+import java.time.temporal.TemporalAccessor
 
-final class ST4TemplateParser extends TemplateParser:
-  override def apply(template: String): Template = ST4Template(ST(template))
+import borscht.template.impl.st4.renderers._
+import borscht.template.{Template, TemplateParser}
+import org.stringtemplate.v4.{AttributeRenderer, ST, STGroup}
+
+final class ST4TemplateParser(group: STGroup) extends TemplateParser:
+  override def apply(template: String): Template = ST4Template(ST(group, template))
+
+object ST4TemplateParser:
+  def apply(group: STGroup): ST4TemplateParser = new ST4TemplateParser(group)
+
+  def apply(renderers: IterableOnce[Renderer[_]] = DefaultAttributeRenderers): ST4TemplateParser =
+    val group = STGroup()
+    renderers.iterator foreach (_(group))
+    ST4TemplateParser(group)
+
+  val DefaultAttributeRenderers: List[Renderer[_]] = List(RendererTemporalAccessor)
