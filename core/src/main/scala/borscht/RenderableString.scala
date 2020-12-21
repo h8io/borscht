7a148490ddf925a9ca1b87f0d27a17c90c8327eb
Eshu <tjano.xibalba@gmail.com>
2020-12-22 01:18:18 +0900

    Unused traits

--- core/src/main/scala/borscht/RenderableString.scala
+++ core/src/main/scala/borscht/RenderableString.scala
@@ -2,7 +2,3 @@ package borscht
 
 trait RenderableString:
   def render: String
-
-trait RenderableMapString(parameters: Map[String, AnyRef]) extends RenderableString
-
-trait RenderableSeqString(parameters: Iterable[AnyRef]) extends RenderableString
