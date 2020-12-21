3375278002ad7088dfa01f9d9032705de47e9038
Eshu <tjano.xibalba@gmail.com>
2020-12-22 01:15:47 +0900

    Java parsers: Locale and MessageFormat

--- core/src/main/scala/borscht/parsers/java.scala
+++ core/src/main/scala/borscht/parsers/java.scala
@@ -2,5 +2,30 @@ package borscht.parsers
 
 import borscht.NodeParser
 
+import java.text.MessageFormat
+import java.util.Locale
+
 given NodeParserClass[T](using parser: NodeParser[String]): NodeParser[Class[T]] =
   parser andThen { name => Class.forName(name).asInstanceOf[Class[T]] }
+
+given NodeParserLocale(using parser: NodeParser[String]): NodeParser[Locale] =
+  parser andThen (new Locale.Builder().setLanguageTag(_).build()) orElse (NodeParserCfgNode andThen { cfg =>
+    cfg.get[String]("variant") map { variant =>
+      Locale(cfg[String]("language"), cfg[String]("country"), variant)
+    } getOrElse {
+      cfg.get[String]("country") map { country =>
+        Locale(cfg[String]("language"), country)
+      } getOrElse {
+        Locale(cfg[String]("language"))
+      }
+    }
+  })
+
+given NodeParserMessageFormat(using parser: NodeParser[String]): NodeParser[MessageFormat] =
+  parser andThen (MessageFormat(_)) orElse (NodeParserCfgNode andThen { cfg =>
+    cfg.get[Locale]("locale") map { locale =>
+      MessageFormat(cfg[String]("pattern"), locale)
+    } getOrElse {
+      MessageFormat(cfg[String]("pattern"))
+    }
+  })
