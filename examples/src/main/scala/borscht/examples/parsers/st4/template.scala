fd8592ac53eb3bff063928dceb4b43ed539852c4
Eshu <tjano.xibalba@gmail.com>
2020-12-25 01:46:32 +0900

    Node parser replaced registry

--- examples/src/main/scala/borscht/examples/parsers/st4/template.scala
+++ examples/src/main/scala/borscht/examples/parsers/st4/template.scala
@@ -1,9 +1,11 @@
 package borscht.examples.parsers.st4
 
 import borscht.NodeParser
-import borscht.template.{NodeParserTemplate, SimpleTemplateParserRegistry, Template}
+import borscht.template.{NodeParserTemplate, NodeParserTemplateParser, Template, TemplateParser}
 import borscht.template.impl.st4.ST4TemplateParser
 
-given NodeParserST4Template: NodeParser[Template] =
-  val st4 = ST4TemplateParser()
-  NodeParserTemplate(SimpleTemplateParserRegistry(st4, "st4" -> st4))
+private val st4 = ST4TemplateParser()
+
+given NodeParserST4TemplateParser: NodeParser[TemplateParser] = NodeParserTemplateParser("st4" -> st4)
+
+given NodeParserST4Template: NodeParser[Template] = NodeParserTemplate(st4)
--- template/core/src/main/scala/borscht/template/NodeParserTemplate.scala
+++ template/core/src/main/scala/borscht/template/NodeParserTemplate.scala
@@ -5,8 +5,9 @@ import borscht.parsers.given
 
 type ParameterParser[T] = String => T
 
-def NodeParserTemplate(registry: TemplateParserRegistry,
-                       parameterParser: TemplateParameterParser = TemplateParameterParser()): NodeParser[Template] =
+def NodeParserTemplate(defaultParser: TemplateParser,
+                       parameterParser: TemplateParameterParser = TemplateParameterParser())
+                      (using nodeParserTemplateParser: NodeParser[TemplateParser]): NodeParser[Template] =
   def toMap(cfg: CfgNode): Map[String, AnyRef] = (cfg.iterator map (_ -> parameter(_))).toMap
 
   def parameter(node: Node): AnyRef = node match
@@ -16,14 +17,8 @@ def NodeParserTemplate(registry: TemplateParserRegistry,
     case iterable: SeqNode => (iterable.iterator map parameter).toArray
     case cfg: CfgNode => toMap(cfg)
 
-  NodeParserPlainString andThen registry(None) orElse
-    (NodeParserCfgNode andThen new PartialFunction[CfgNode, Template] {
-      override def isDefinedAt(cfg: CfgNode): Boolean = cfg.child("parameters") map {
-        case _: CfgNode => true
-        case _ => false
-      } getOrElse true
-
-      override def apply(cfg: CfgNode): Template =
-        registry(cfg.get[String]("parser"))(NodeParserPlainString(cfg[ScalarNode]("template")))
-          .set(toMap(cfg[CfgNode]("parameters")))
+  NodeParserPlainString andThen defaultParser orElse
+    (NodeParserCfgNode andThen { cfg =>
+      val parser = cfg.get[TemplateParser]("parser") getOrElse defaultParser
+      parser(NodeParserPlainString(cfg[ScalarNode]("template"))).set(toMap(cfg[CfgNode]("parameters")))
     })
