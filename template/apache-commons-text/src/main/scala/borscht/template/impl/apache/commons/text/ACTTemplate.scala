b82d7436b91c566a882585369702d9f8fcad9124
Eshu <tjano.xibalba@gmail.com>
2021-01-09 12:54:53 +0900

    Refactoring

--- template/apache-commons-text/src/main/scala/borscht/template/impl/apache/commons/text/ACTTemplate.scala
+++ template/apache-commons-text/src/main/scala/borscht/template/impl/apache/commons/text/ACTTemplate.scala
@@ -2,11 +2,13 @@ package borscht.template.impl.apache.commons.text
 
 import borscht.template.Template
 import borscht.template.impl.apache.commons.text.ph.parse
-import borscht.template.impl.apache.commons.text.renderers.{DefaultRenderer, Renderer, TemporalRenderer}
+import borscht.template.impl.apache.commons.text.renderers.{Renderer, TemporalRenderer}
 import org.apache.commons.text.StringSubstitutor
 import org.apache.commons.text.lookup.{StringLookup, StringLookupFactory}
 
-import java.util.Locale
+import java.text.{DateFormat, NumberFormat}
+import java.time.temporal.TemporalAccessor
+import java.util.{Calendar, Date, Formatter, Locale}
 import scala.jdk.CollectionConverters._
 
 private[text] final class ACTTemplate(substitutor: StringSubstitutor,
@@ -29,5 +31,39 @@ private[text] final class ACTTemplate(substitutor: StringSubstitutor,
 
   override def lookup(ph: String): String =
     val (key, vf) = parse(ph, valueFormat)
-    DefaultRenderer(renderer, vf, parameters.get(key).orNull)
-  
+    render(vf, parameters.get(key).orNull)
+
+  private def render(vf: ValueFormat, value: AnyRef): String = renderer(vf, value) getOrElse {
+    value match
+      case null | None => vf.nullValue
+      case Some(value) => render(vf, value.asInstanceOf[AnyRef])
+      case _ => vf.format map { fmt =>
+        value match
+          case _: (Character | CharSequence | java.lang.Number | Date | Calendar | TemporalAccessor) =>
+            val formatter = Formatter(vf.locale)
+            try formatter.format(fmt, value).toString finally formatter.close()
+          case seq: Iterable[_] =>
+            render(vf, seq.iterator, vf.seqStart, vf.seqSeparator, vf.seqEnd)
+          case product: Product =>
+            render(vf, product.productIterator, vf.productStart, vf.productSeparator, vf.productEnd)
+          case _ => throw IllegalArgumentException(s"Unformattable value $value class ${value.getClass}")
+      } getOrElse {
+        value match
+          case n: Number => NumberFormat.getInstance(vf.locale).format(n)
+          case cs: CharSequence => cs.toString
+          case temporal: TemporalAccessor => borscht.util.render(temporal, None, vf.locale)
+          case date: Date => render(date, vf.locale)
+          case calendar: Calendar => render(calendar.getTime, vf.locale)
+          case seq: Iterable[_] =>
+            render(vf, seq.iterator, vf.seqStart, vf.seqSeparator, vf.seqEnd)
+          case product: Product =>
+            render(vf, product.productIterator, vf.productStart, vf.productSeparator, vf.productEnd)
+          case _ => value.toString
+      }
+  }
+
+  private def render(date: Date, locale: Locale): String =
+    DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, locale).format(date)
+
+  private def render(vf: ValueFormat, seq: Iterator[_], start: String, separator: String, end: String): String =
+    seq map { item => render(vf, item.asInstanceOf[AnyRef]) } mkString (start, separator, end)
