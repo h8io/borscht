ff56d463ad43a1582fd7428dc7a9545920b2048a
Eshu <tjano.xibalba@gmail.com>
2021-05-02 10:14:34 +0900

    Parser tests

--- typed/core/src/main/scala/borscht/typed/ValueTypeParserException.scala
+++ typed/core/src/main/scala/borscht/typed/ValueTypeParserException.scala
@@ -3,5 +3,5 @@ package borscht.typed
 class ValueTypeParserException(position: Int, message: String, cause: Option[Exception] = None)
   extends RuntimeException(message + "@" + position, cause.orNull)
 
-class UnexpectedEventException(event: Event)
+class UnexpectedEventException(val event: Event)
   extends ValueTypeParserException(event.position, s"Unexpected event $event")
--- typed/core/src/test/scala/borscht/typed/ValueTypeParserTest.scala
+++ typed/core/src/test/scala/borscht/typed/ValueTypeParserTest.scala
@@ -3,7 +3,7 @@ package borscht.typed
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class ValueTypeParserTest extends AnyFlatSpec with Matchers {
+class ValueTypeParserTest extends AnyFlatSpec with Matchers :
   private val types: Map[String, ValueTypeConstructor] =
     Iterator("abc", "def", "ghi", "jkl", "mno").map(name => name -> TestValueTypeConstructor(name)).toMap
 
@@ -20,4 +20,38 @@ class ValueTypeParserTest extends AnyFlatSpec with Matchers {
           TestValueType("ghi", List(TestValueType("jkl", Nil))))),
         TestValueType("mno", Nil)))
   }
-}
+
+  it should "throw exteption for the single comma in the empty parameter list" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[,]")).event shouldBe
+      Event.TypeListSeparator(4)
+  }
+
+  it should "throw exteption for the extra comma in the beginning of a parameter list" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[,def, ghi]")).event shouldBe
+      Event.TypeListSeparator(4)
+  }
+
+  it should "throw exteption for the extra comma in the end of a parameter list" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[def, ghi,]")).event shouldBe
+      Event.TypeListEnd(13)
+  }
+
+  it should "throw exteption for the double comma" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[def,, ghi]")).event shouldBe
+      Event.TypeListSeparator(8)
+  }
+
+  it should "throw exteption for double left square brackets" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[[def, ghi]")).event shouldBe
+      Event.TypeListStart(4)
+  }
+
+  it should "throw exteption for unbalanced left square brackets" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[def[ghi, jkl]")).event shouldBe
+      Event.End(17)
+  }
+
+  it should "throw exteption for unbalanced right square brackets" in {
+    (the[UnexpectedEventException] thrownBy ValueTypeParser(types)("abc[def, ghi]]")).event shouldBe
+      Event.TypeListEnd(13)
+  }
