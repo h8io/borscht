811f8df1cf4cb3413850e09faa9e2e3f67c0bf01
Eshu <tjano.xibalba@gmail.com>
2021-05-23 13:49:54 +0900

    Redundant decomposition

--- typed/core/src/main/scala/borscht/typed/events/TypeParser.scala
+++ typed/core/src/main/scala/borscht/typed/events/TypeParser.scala
@@ -5,7 +5,7 @@ import borscht.typed.{ValueType, ValueTypeConstructor}
 private[events] class TypeParser(parent: UpdatableParser[ValueType],
                                  types: PartialFunction[String, ValueTypeConstructor]) extends Parser :
   override def apply(event: Event): Parser = event match
-    case event@Event.TypeName(name, position) =>
-      val constructor = types.applyOrElse(name, _ => throw UnknownType(event))
+    case event: Event.TypeName =>
+      val constructor = types.applyOrElse(event.value, _ => throw UnknownType(event))
       ParametersParser(parent, constructor, types)
     case unexpected => throw UnexpectedEvent(unexpected)
