f60727fea223e71b3d5243251c13cfa36df1c5f3
Eshu <tjano.xibalba@gmail.com>
2021-07-31 23:57:31 +0900

    Start parser implementation

--- typed/src/main/scala/borscht/typed/CfgNodeParserTypedValue.scala
+++ typed/src/main/scala/borscht/typed/CfgNodeParserTypedValue.scala
@@ -7,9 +7,12 @@ import borscht.reflect.ComponentRef
 class CfgNodeParserTypedValue(val types: Map[String, ValueType]) extends NodeParser[TypedValue]:
   def this(types: CfgNode) = this(types[Map[String, ComponentRef[ValueType]]]() map (_ -> _.get))
 
-  def isDefinedAt(node: Node): Boolean = ???
+  def isDefinedAt(node: Node): Boolean = node match
+    case cfg: CfgNode => ???
+    case _ => false
 
-  def apply(node: Node): TypedValue = ???
+  def apply(node: Node): TypedValue = (node: @unchecked) match
+    case cfg: CfgNode => ???
 
   override def orElse[N <: Node, V >: TypedValue](that: PartialFunction[N, V]): PartialFunction[N, V] = that match
     case that: CfgNodeParserTypedValue => CfgNodeParserTypedValue(that.types ++ types)
