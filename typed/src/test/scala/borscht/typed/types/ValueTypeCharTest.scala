54ca13a8945bdc5c0bd20e1d6865e101a6818767
Eshu <tjano.xibalba@gmail.com>
2021-08-03 22:58:30 +0900

    Char value parser test

---
+++ typed/src/test/scala/borscht/typed/types/ValueTypeCharTest.scala
@@ -0,0 +1,14 @@
+package borscht.typed.types
+
+import borscht.test.scalar
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.util.Right
+
+class ValueTypeCharTest extends AnyFlatSpec with Matchers:
+  "Char value type" should "parse characters correctly" in {
+    ValueTypeChar()(Nil) map (_(scalar("*"))) shouldEqual Right('*')
+    ValueTypeChar()(Nil) map (_(scalar(48))) shouldEqual Right('0')
+    ValueTypeChar()(Nil) map (_(scalar("\\u0020"))) shouldEqual Right(' ')
+  }
