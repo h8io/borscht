06ec7183398859450bf5d388428c5585f27f2d58
Eshu <tjano.xibalba@gmail.com>
2021-08-06 08:46:51 +0900

    JChar parser

--- typed/src/main/scala/borscht/typed/types/char.scala
+++ typed/src/main/scala/borscht/typed/types/char.scala
@@ -6,15 +6,25 @@ import borscht.typed.ValueParser
 
 private val EncodedCharPattern = raw"\\u([\d+A-Fa-f]{4})".r
 
-final class ValueTypeChar extends ValueTypeParameterless with StringParser[Char]:
-  override protected def parser(nothing: Unit): ValueParser = node => node match
+private object CharParser:
+  def parser: ValueParser = node => node match
     case scalar: ScalarNode => scalar.value match
       case value: Char => value
+      case value: Character => value.charValue
       case value: Int => value.toChar
       case _ => parse(node.parse[String])
     case _ => parse(node.parse[String])
 
-  override def parse(value: String): Char = if (value.length == 1) value.head else value match {
+  def parse(value: String): Char = if (value.length == 1) value.head else value match
     case EncodedCharPattern(code) => Integer.parseInt(code, 16).toChar
     case _ => throw MatchError(value)
-  }
+
+final class ValueTypeChar extends ValueTypeParameterless with StringParser[Char]:
+  override protected def parser(nothing: Unit): ValueParser = CharParser.parser
+
+  override def parse(value: String): Char = CharParser.parse(value)
+
+final class ValueTypeJChar extends ValueTypeParameterless with StringParser[Character]:
+  override protected def parser(nothing: Unit): ValueParser = CharParser.parser
+
+  override def parse(value: String): Character = CharParser.parse(value)
