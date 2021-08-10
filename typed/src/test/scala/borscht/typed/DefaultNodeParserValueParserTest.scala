6ff138b159764c91c78c46b28b25aca2c99b6331
Eshu <tjano.xibalba@gmail.com>
2021-08-10 21:33:47 +0900

    DefaultNodeParserValueParser test

--- typed/src/test/scala/borscht/typed/DefaultNodeParserValueParserTest.scala
+++ typed/src/test/scala/borscht/typed/DefaultNodeParserValueParserTest.scala
@@ -1,11 +1,18 @@
 package borscht.typed
 
-import borscht.test.cfg
-
+import borscht.test.scalar
+import borscht.typed.types.{TestValueParser, TestValueType}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class DefaultNodeParserValueParserTest extends AnyFlatSpec with Matchers:
   "DefaultNodeParserValueParser" should "parse type correctly" in {
-    
+    val parser = DefaultNodeParserValueParser(Map[String, ValueType](
+      "type1" -> TestValueType("type1"), "type2" -> TestValueType("type2")))
+    parser(scalar("type1[type2, type1[type2, type1]]")) shouldEqual
+      TestValueParser("type1", List(
+        TestValueParser("type2", Nil),
+        TestValueParser("type1", List(
+          TestValueParser("type2", Nil),
+          TestValueParser("type1", Nil)))))
   }
--- typed/src/test/scala/borscht/typed/types/TestValueType.scala
+++ typed/src/test/scala/borscht/typed/types/TestValueType.scala
@@ -2,7 +2,7 @@ package borscht.typed.types
 
 import borscht.typed.{AbstractValueType, ValueParser}
 
-class TestValueType(name: String) extends AbstractValueType[List[ValueParser]]:
+case class TestValueType(name: String) extends AbstractValueType[List[ValueParser]]:
   override protected def prepare(parameters: List[ValueParser]): Either[String, List[ValueParser]] = Right(parameters)
 
   override protected def create(parameters: List[ValueParser]): ValueParser = TestValueParser(name, parameters)
