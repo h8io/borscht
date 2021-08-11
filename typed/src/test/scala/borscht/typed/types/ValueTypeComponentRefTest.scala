50eb63eb1d99d1600460c93bd8c7730316050df9
Eshu <tjano.xibalba@gmail.com>
2021-08-11 22:31:39 +0900

    Conponent reference value type

--- typed/src/test/scala/borscht/typed/types/ValueTypeComponentRefTest.scala
+++ typed/src/test/scala/borscht/typed/types/ValueTypeComponentRefTest.scala
@@ -9,7 +9,7 @@ import scala.util.Right
 class ValueTypeComponentRefTest extends AnyFlatSpec with Matchers:
   "Component reference value type" should "create a correct component instance" in {
     ValueTypeComponentRef().parser(Nil) map (_(cfg("class" -> "java.lang.RuntimeException",
-      "parameters" -> seq("Exception component")))) match
+      "parameters" -> "Exception component"))) match
       case Right(e: RuntimeException) => e.getMessage shouldEqual "Exception component"
       case unexpected => fail(s"Unexpected component $unexpected")
   }
